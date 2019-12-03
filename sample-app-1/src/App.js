2f1e08f661431b9af2ddb09ac5475681fb4ca053
Ayaka Tanabe <tanabe0kitkat@gmail.com>
2019-12-03 12:57:17 +0900

    共有用

---
+++ sample-app-1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseButton from './components/button/Button'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseButton>こんにちは</BaseButton>
+    </div>
+  );
+}
+
+export default App;

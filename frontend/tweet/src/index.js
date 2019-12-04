434b6f0619eee1e1259c9acd30612e0a797be490
Ayaka Tanabe <t1820018@edu.cc.uec.ac.jp>
2019-12-05 01:45:08 +0900

    create-react-appしてyarn addした

--- frontend/tweet/src/index.js
+++ frontend/tweet/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "normalize.css";
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

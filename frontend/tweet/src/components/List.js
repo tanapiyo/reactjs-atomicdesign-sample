6ecb0adc956bacf72175cc5702abb6b973cdea6b
Ayaka Tanabe <t1820018@edu.cc.uec.ac.jp>
2019-12-14 21:56:41 +0900

    add memo about prettier

--- frontend/tweet/src/components/List.js
+++ frontend/tweet/src/components/List.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
 
-import SideMenu from './SideMenu/SideMenu'
-import Trend from './Trends/Trend'
-import TweetMain from './Tweets/TweetMain'
-import Container from '@material-ui/core/Container';
+import SideMenu from "./SideMenu/SideMenu";
+import Trend from "./Trends/Trend";
+import TweetMain from "./Tweets/TweetMain";
+import Container from "@material-ui/core/Container";
 
 const List = () => {
-    return(
-        <Container fixed>
-            <Grid container spacing={3}>
-                <Grid item xs={3}><SideMenu /></Grid>
-                <Grid item xs={6}><TweetMain /></Grid>
-                <Grid item xs={3}><Trend /></Grid> 
-            </Grid>
-        </Container>
- 
-    );
+  return (
+    <Container fixed>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <SideMenu />
+        </Grid>
+        <Grid item xs={6}>
+          <TweetMain />
+        </Grid>
+        <Grid item xs={3}>
+          <Trend />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default List;
--- frontend/tweet/src/util/theme.js
+++ frontend/tweet/src/util/theme.js
@@ -1,49 +1,48 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 export default createMuiTheme({
   palette: {
     primary: {
-        main: '#42a5f5',
-        light: '#e3f2fd',//for background
+      main: "#42a5f5",
+      light: "#e3f2fd" //for background
     },
     secondary: {
-        light: '#9e9e9e',
-        main: '#616161',//copyrightとか
+      light: "#9e9e9e",
+      main: "#616161" //copyrightとか
     }
   },
   typography: {
     fontFamily: [
-      'Arial',
-      'ヒラギノ角ゴ Pro W3',
-      'Hiragino Kaku Gothic Pro',
-      'Osaka',
-      'メイリオ'
-    ].join(','),
+      "Arial",
+      "ヒラギノ角ゴ Pro W3",
+      "Hiragino Kaku Gothic Pro",
+      "Osaka",
+      "メイリオ"
+    ].join(","),
     fontSize: 12,
     subtitle1: {
-    fontSize: 12,
+      fontSize: 12
     },
     h3: {
-        fontSize: 18,
-        fontWeight: 700,
+      fontSize: 18,
+      fontWeight: 700
     },
     button: {
-        fontSize: 14,
-        fontWeight: 500,
+      fontSize: 14,
+      fontWeight: 500
     },
     body2: {
-        fontSize: 10,
-        color: '#616161',
-    },
-    
+      fontSize: 10,
+      color: "#616161"
+    }
   },
   overrides: {
     MuiButton: {
       root: {
         padding: 15,
-        '&:hover': {
-          backgroundColor: '#e3f2fd',
-          color: '#42a5f5'
+        "&:hover": {
+          backgroundColor: "#e3f2fd",
+          color: "#42a5f5"
         }
       }
     }
